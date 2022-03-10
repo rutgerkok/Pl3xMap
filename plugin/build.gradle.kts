708cd08e1efe1b596f3cbc65fe9fcb909f749e0e
NeumimTo <neumimto@users.noreply.github.com>
2022-03-10 08:46:54 +0100

    update command framework

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
     implementation(project(":pl3xmap-api"))
-    val cloudVersion = "1.5.0"
+    val cloudVersion = "1.6.2"
     implementation("cloud.commandframework", "cloud-paper", cloudVersion)
     implementation("cloud.commandframework", "cloud-minecraft-extras", cloudVersion)
     implementation("net.kyori", "adventure-text-minimessage", "4.10.0")
